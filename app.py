83ba1f137a475630abe670d6e29d5a5e387936f2
cezol <90487226+cezol@users.noreply.github.com>
2025-08-05 10:07:59 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,114 @@
+import requests
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+from modules.session import UserSession
+from modules.menu import MenuManager
+from modules.document import DocumentHandler
+from config import db_users, db_lista, OPENAI_API_KEY, TWILIO_AUTH_TOKEN, TWILIO_SID, PROMPT_FILTRAGEM, \
+    PROMPT_AUDIO_REMOVE, PROMPT_AUDIO_ADD, acesso_pdf, PROMPT_TELEFONE
+import openai
+
+openai.api_key = OPENAI_API_KEY
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def reply():
+    number = (request.form.get("From") or "").replace("whatsapp:+5527", "")
+    text = (request.form.get("Body") or "").strip()
+    media_url = request.form.get("MediaUrl0", "")
+    media_type = request.form.get("MediaContentType0", "")
+    num_media = int(request.form.get("NumMedia", 0))
+    res = MessagingResponse()
+    user = UserSession(number, db_users)
+    if not user.is_authorized():
+        return str(res)
+    if text == "..":
+        user.toggle_bot(res)
+        if user.is_bot_active():
+            res.message(MenuManager.menu_principal(user.get_loja(), db_lista,user.get_nome()))
+            user.set_status("main")
+        return str(res)
+    if not user.is_bot_active():
+        return str(res)
+    if media_url and ("pdf" in media_type.lower() or "image" in media_type.lower()) and user.get_nome() in acesso_pdf:
+        res.message(DocumentHandler.handle_upload(media_type, media_url, res, user))
+        return str(res)
+    if media_url and "audio" in media_type:
+        try:
+            response = requests.get(media_url, auth=(TWILIO_SID, TWILIO_AUTH_TOKEN))
+            response.raise_for_status()
+            audio_data = response.content
+            audio_path = "/tmp/audio.ogg"
+            with open(audio_path, "wb") as f:
+                print('5')
+                f.write(audio_data)
+            with open(audio_path, "rb") as audio_file:
+                transcript = openai.audio.transcriptions.create(
+                    model="whisper-1",
+                    file=audio_file
+                )
+                text = transcript.text
+            gpt_response = openai.chat.completions.create(
+                model="gpt-4",
+                messages=[
+                    {"role": "system",
+                     "content": PROMPT_FILTRAGEM},
+                    {"role": "user",
+                     "content": f"O usuário disse: '{text}'"}
+                ]
+            )
+            option = gpt_response.choices[0].message.content.strip()
+            if option == 'adicionar':
+                gpt_response = openai.chat.completions.create(
+                    model="gpt-4",
+                    messages=[
+                        {"role": "system",
+                         "content": PROMPT_AUDIO_ADD},
+                        {"role": "user",
+                         "content": f"O usuário disse: '{text}'. Interprete como um item para adicionar à lista de compras, sem explicações."}
+                    ]
+                )
+                text = gpt_response.choices[0].message.content.strip()
+                MenuManager._handle_lista_add(text, res, user, db_lista)
+            elif option == 'remover':
+                gpt_response = openai.chat.completions.create(
+                    model="gpt-4",
+                    messages=[
+                        {"role": "system",
+                         "content": PROMPT_AUDIO_REMOVE},
+                        {"role": "user",
+                         "content": f"O usuário disse: '{text}'. Interprete como um item para remover de uma lista de compras, sem explicações."}
+                    ]
+                )
+                text = gpt_response.choices[0].message.content.strip()
+                MenuManager.handle_remove_item(text, res, user, db_lista)
+            elif option == 'limpar':
+                MenuManager.clear_lista(res, user, db_lista)
+            elif option == 'telefone':
+                gpt_response = openai.chat.completions.create(
+                    model="gpt-4",
+                    messages=[
+                        {"role": "system",
+                         "content": PROMPT_TELEFONE},
+                        {"role": "user",
+                         "content": f"O usuário disse: '{text}'"}
+                    ]
+                )
+                nome = gpt_response.choices[0].message.content.strip()
+                # res.message(MenuManager.buscarTelefones(nome))
+                ENCONTRADOS = MenuManager.buscarTelefones(nome)
+                if ENCONTRADOS:
+                    for i, (nome, telefone) in enumerate(ENCONTRADOS, 1):
+                        res.message((f"{i}. {nome} - {telefone}"))
+                else:
+                    res.message('nao encontrado')
+            return str(res)
+        except Exception as e:
+            res.message(f"❌ Erro ao transcrever áudio: {e}")
+            return str(res)
+
+    return MenuManager.process_input(text, res, user, db_lista)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ config.py
@@ -0,0 +1,140 @@
+# config.py
+
+
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+acesso_pdf = ['LUIZ','JCB','JCBF']
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_SID = os.getenv("TWILIO_SID")
+IMGBB_API_KEY = os.getenv("IMGBB_API_KEY")
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+
+GESTAO_Content_Type = os.getenv("GESTAO_Content_Type")
+GESTAO_access_token = os.getenv("GESTAO_access_token")
+GESTAO_secret_access_token = os.getenv("GESTAO_secret_access_token")
+GESTAO_base_url = os.getenv("GESTAO_base_url")
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+cluster = MongoClient(MONGODB_URI, tls=True, tlsAllowInvalidCertificates=True)
+db = cluster["Agrofatima"]
+db_users = db["users"]
+db_lista = db["lista_de_compras"]
+PROMPT_DOC2 = """Analise o documento fornecido e extraia as seguintes informações de forma estruturada:
+
+ara cada nota fiscal, gere um dicionario python separado com os campos:
+
+{
+  "cpf_cnpj": "<CNPJ ou CPF do *EMISSOR*>",
+  "valor_produtos": <float>,
+  "frete": <float>,
+  "outras_despesas": <float>,
+  "desconto": <float>,
+  "valor_total_nota": <float>,
+  "DANFE": "<número da nota se houver>",
+  "data_emissao": "<yyyy-mm-dd>",
+  "pagamentos": [
+    {"valor": <float>, "vencimento": "<yyyy-mm-dd>"},
+    ...
+  ]
+}
+
+Regras:
+- Todos os valores devem estar no formato numérico (float), sem aspas.
+- Se houver mais de um pagamento ou fatura, inclua todos no array `pagamentos`.
+- Se não houver uma informação, coloque '0'.
+- Retorne **apenas o dicionario python**, sem explicações, cabeçalhos ou comentários.
+
+Se o documento não contiver notas fiscais, retorne uma lista vazia: `[]`.
+
+"""
+PROMPT_DOC = """Analise o documento fornecido e extraia as seguintes informações na ordem:
+1. Tipo de documento (NF, NFS, BL)
+2. CNPJ ou CPF do emissor
+3. Valor líquido
+4. Valor bruto
+5. Data de emissão (DD/MM/AAAA)
+6. Data de vencimento (DD/MM/AAAA)
+7. Número DANFE
+
+Se não encontrar algum campo, use "None".
+
+⚠️ A resposta deve ser apenas uma linha, em formato CSV, sem cabeçalho, sem comentários, sem aspas, sem colchetes e 
+sem qualquer texto adicional. Apenas os dados, separados por vírgulas.
+"""
+PROMPT_AUDIO_ADD = """A partir do áudio recebido, extraia os itens agrícolas solicitados para compra, no contexto de uma fazenda de produção de café.
+
+Para cada item mencionado, identifique:
+- O nome do item (em letras minúsculas).
+- A quantidade associada (com unidade, se aplicável).
+
+⚠️ Responda com uma lista de dicionários no formato Python, como no exemplo abaixo:
+
+[
+  {"item": "adubo", "quantidade": "10 sacos"},
+  {"item": "sementes de café", "quantidade": "5 kg"},
+  {"item": "herbicida", "quantidade": "2 litros"}
+]
+
+⚠️ Não adicione explicações, nem texto adicional. Apenas a lista."""
+PROMPT_AUDIO_REMOVE = """A partir do áudio recebido, extraia os itens agrícolas solicitados para remocao de uma lista, no contexto de uma fazenda de produção de café.
+
+Para cada item mencionado, identifique:
+- O nome do item (em letras minúsculas).
+- A quantidade associada (se aplicável).
+
+⚠️ Responda com uma lista de dicionários no formato Python,tudo em letra minuscula, como no exemplo abaixo:
+
+[
+  {"item": "adubo", "quantidade": "10 sacos"},
+  {"item": "sementes de café", "quantidade": "5 kg"},
+  {"item": "herbicida", "quantidade": "2 litros"}
+]
+
+⚠️ Não adicione explicações, nem texto adicional. Apenas a lista."""
+PROMPT_FILTRAGEM = """
+Você é um assistente de uma empresa agrícola que recebe comandos por áudio.
+
+Sua tarefa é identificar qual é a intenção principal do usuário com base no texto transcrito do áudio. As intenções possíveis são:
+
+- "adicionar": se o usuário estiver dizendo o que quer comprar, como produtos, insumos agrícolas etc.
+- "remover": se o usuário estiver pedindo para retirar ou apagar algum item da lista de compras.
+- "limpar": se o usuário estiver pedindo para apagar, limpar, deletar toda a lista. 
+- "telefone": se o usuário estiver pedindo o número de telefone de um colaborador.
+⚠️ Responda com **apenas** uma dessas quatro palavras: adicionar, remover, limpar ou telefone.
+"""
+PROMPT_TELEFONE = """
+A partir da mensagem de voz recebida, extraia o nome completo do colaborador para o qual está sendo solicitado o telefone.
+
+📌 Considere que o nome pode ser dito de forma parcial, composta ou com detalhes fonéticos, como:
+- "com Z"
+- "com S"
+- "com dois Ls"
+- "termina com TH"
+- "com H mudo"
+- "com C e não com K"
+Essas pistas devem ser consideradas para tentar identificar a grafia mais provável do nome, mesmo que nem todas as letras sejam claramente pronunciadas.
+
+⚠️ Extraia somente o nome mencionado, sem o cargo, nem outros dados.  
+⚠️ A resposta deve conter apenas uma lista Python com **cada parte do nome** como strings separadas e **em letras minúsculas**, sem pontuação.
+
+📌 Exemplo:
+
+Entrada (áudio):  
+"Quero o telefone do João Vítor com dois tês e h no final"
+
+Saída:
+["joao", "vittorh"]
+
+Entrada:  
+"Telefone do Julio Cezar, com z e sem acento"
+
+Saída:
+["julio", "cezar"]
+
+⚠️ Não adicione explicações ou texto adicional. Apenas a lista, no formato exato solicitado.
+"""
+
