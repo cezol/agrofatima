b291d8a275feff43e36b7eac886e90703d053f66
cezol <90487226+cezol@users.noreply.github.com>
2025-08-06 09:44:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def reply():
     num_media = int(request.form.get("NumMedia", 0))
     res = MessagingResponse()
     user = UserSession(number, db_users)
+    print(user.get_nome())
     if user.get_nome() == 'JCBF':
         lista = db_lista_sinezia
     else:
@@ -116,3 +117,4 @@ def reply():
 
 if __name__ == "__main__":
     app.run()
+
